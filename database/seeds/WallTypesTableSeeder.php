a418f6b6093b2e4314cf63478e0033d7a11fd679
Valerii R <vr@gmail.com>
2017-06-10 12:39:08 +0300

    create WallTypes model, change WallTypesTableSeeder

--- database/seeds/WallTypesTableSeeder.php
+++ database/seeds/WallTypesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\WallType;
 
 class WallTypesTableSeeder extends Seeder
 {
@@ -11,67 +12,80 @@ class WallTypesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('wall_types')->insert([
-            [
-                'name' => 'кирпич',
-                'display_name' => 'кирпич',
-            ],
-            [
-                'name' => 'силикатный кирпич',
-                'display_name' => 'силикатный кирпич',
-            ],
-            [
-                'name' => 'панель',
-                'display_name' => 'панель',
-            ],
-            [
-                'name' => 'пеноблок',
-                'display_name' => 'пеноблок',
-            ],
-            [
-                'name' => 'монолит',
-                'display_name' => 'монолит',
-            ],
-            [
-                'name' => 'ракушечник (ракушняк)',
-                'display_name' => 'ракушечник (ракушняк)',
-            ],
-            [
-                'name' => 'монолитно-кирпичный',
-                'display_name' => 'монолитно-кирпичный',
-            ],
-            [
-                'name' => 'монолитно-блочный',
-                'display_name' => 'монолитно-блочный',
-            ],
-            [
-                'name' => 'дерево и кирпич',
-                'display_name' => 'дерево и кирпич',
-            ],
-            [
-                'name' => 'инкерманский камень',
-                'display_name' => 'инкерманский камень',
-            ],
-            [
-                'name' => 'бутовый камень',
-                'display_name' => 'бутовый камень',
-            ],
-            [
-                'name' => 'газобетон',
-                'display_name' => 'газобетон',
-            ],
-            [
-                'name' => 'керамзитобетон',
-                'display_name' => 'керамзитобетон',
-            ],
-            [
-                'name' => 'монолитно-каркасный',
-                'display_name' => 'монолитно-каркасный',
-            ],
-            [
-                'name' => 'керамический блок',
-                'display_name' => 'керамический блок',
-            ],
+        WallType::create([
+            'name' => 'кирпич',
+            'display_name' => 'кирпич',
         ]);
+
+        WallType::create([
+            'name' => 'силикатный кирпич',
+            'display_name' => 'силикатный кирпич',
+        ]);
+
+        WallType::create([
+            'name' => 'панель',
+            'display_name' => 'панель',
+        ]);
+
+        WallType::create([
+            'name' => 'пеноблок',
+            'display_name' => 'пеноблок',
+        ]);
+
+        WallType::create([
+            'name' => 'монолит',
+            'display_name' => 'монолит',
+        ]);
+
+        WallType::create([
+            'name' => 'ракушечник (ракушняк)',
+            'display_name' => 'ракушечник (ракушняк)',
+        ]);
+
+        WallType::create([
+            'name' => 'монолитно-кирпичный',
+            'display_name' => 'монолитно-кирпичный',
+        ]);
+
+        WallType::create([
+            'name' => 'монолитно-блочный',
+            'display_name' => 'монолитно-блочный',
+        ]);
+
+        WallType::create([
+            'name' => 'дерево и кирпич',
+            'display_name' => 'дерево и кирпич',
+        ]);
+
+        WallType::create([
+            'name' => 'инкерманский камень',
+            'display_name' => 'инкерманский камень',
+        ]);
+
+        WallType::create([
+            'name' => 'бутовый камень',
+            'display_name' => 'бутовый камень',
+        ]);
+
+        WallType::create([
+            'name' => 'газобетон',
+            'display_name' => 'газобетон',
+        ]);
+
+        WallType::create([
+            'name' => 'керамзитобетон',
+            'display_name' => 'керамзитобетон',
+        ]);
+
+        WallType::create([
+            'name' => 'монолитно-каркасный',
+            'display_name' => 'монолитно-каркасный',
+        ]);
+
+        WallType::create([
+            'name' => 'керамический блок',
+            'display_name' => 'керамический блок',
+        ]);
+
     }
 }
