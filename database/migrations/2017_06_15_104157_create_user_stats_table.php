14512d3324ea8d44cbf7edb60678d5145a59a483
Valerii R <vr@gmail.com>
2017-06-15 17:52:05 +0300

    add Exchange_rate and User_stats tables

---
+++ database/migrations/2017_06_15_104157_create_user_stats_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserStatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_stats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->date('date');
+            $table->integer('cabinet_spend_time')->default(0);
+            $table->integer('offered_properties')->default(0);
+            $table->integer('added_clients')->default(0);
+            $table->integer('added_posts')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_stats');
+    }
+}
